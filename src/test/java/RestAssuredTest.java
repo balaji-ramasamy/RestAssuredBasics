da4e6db648f7f0756dba88afa22919117ae13b27
balaji ramasamy <baramasa27@gmail.com>
2022-05-17 19:54:08 +0530

    added tests for logging

--- src/test/java/RestAssuredTest.java
+++ src/test/java/RestAssuredTest.java
@@ -1,3 +1,6 @@
+import io.restassured.config.LogConfig;
+import io.restassured.path.json.JsonPath;
+import io.restassured.response.Response;
 import org.testng.annotations.Test;
 import static io.restassured.RestAssured.*;
 import static org.hamcrest.Matchers.*;
@@ -35,7 +38,107 @@ public class RestAssuredTest {
                 .statusCode(200)
                 .body("workspaces.name", hasItems("My Workspace", "New workspace 1", "New Workspace 2"));
     }
-    
+
+    @Test
+    public void assertExtractResponseTest() {
+
+        Response response = given().
+                baseUri(baseURL).
+                header("x-api-key",apiKey).
+                log().all().
+        when().
+                get("/workspaces").
+        then().
+                log().all()
+                .extract()
+                .response();
+
+        System.out.println(response.asString());
+
+    }
+
+    @Test
+    public void extractSingleFieldTest() {
+
+       String response = given().
+                baseUri(baseURL)
+                .header("x-api-key", apiKey)
+                .log().all().
+        when()
+                .get("/workspaces").
+                then().log().all()
+                .extract().response().asString();
+
+       //method1:
+        //System.out.println((String) response.path("workspaces[0].name"));
+
+        //method2:
+        JsonPath jsonPath = new JsonPath(response);
+        System.out.println(jsonPath.getString("workspaces[0].name"));
+
+
+        //Method3:
+        System.out.println(JsonPath.from(response).getString("workspaces[0].name"));
+
+    }
+
+    @Test
+    public void reqAndRespLoggingTest() {
+        given().
+                baseUri(baseURL)
+                .header("x-api-key", apiKey)
+                .log().all()
+        .when()
+                .get("/workspaces")
+        .then()
+                .log().body();
+
+    }
+
+    @Test
+    public void logIfErrorTest() {
+        given().
+                baseUri(baseURL)
+                .header("x-api-key",apiKey)
+                .when()
+                .get("/workspaces")
+                .then()
+                .log()
+                .ifError()
+                .assertThat().
+                statusCode(200);
+
+    }
+
+    @Test
+    public void logIfValidationFailsTest() {
+       given()
+                .baseUri(baseURL)
+                .header("x-api-key", apiKey)
+                .config(config().logConfig(LogConfig.logConfig().enableLoggingOfRequestAndResponseIfValidationFails()))
+                .when()
+                .get("/workspaces")
+                .then()
+                .assertThat().statusCode(201);
+
+    }
+
+
+    @Test
+    public void blackListHeaderTest() {
+
+        given()
+                .baseUri(baseURL)
+                .header("x-api-key",apiKey)
+                .config(config().logConfig(LogConfig.logConfig().blacklistHeader("x-api-key")))
+                .log().headers()
+                .when()
+                .get("/workspaces")
+                .then()
+                .assertThat()
+                .statusCode(200);
+
+    }
 
 
 
