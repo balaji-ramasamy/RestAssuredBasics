959dc4f5815e6d412a00dcb03829c0f62efb9b73
balaji ramasamy <baramasa27@gmail.com>
2022-05-17 07:14:40 +0530

    added test for asserting status code

---
+++ src/test/java/RestAssuredTest.java
@@ -0,0 +1,23 @@
+import org.testng.annotations.Test;
+import static io.restassured.RestAssured.*;
+
+public class RestAssuredTest {
+
+    private static final String baseURL = "https://api.getpostman.com";
+    private static final String apiKey ="PMAK-627c5ad78bbfca15225264ca-b5a81b2feedf37166c56809f39cddbecbe";
+
+    @Test
+    public void assertStatusCodeTest(){
+        given().
+                baseUri(baseURL).
+                header("x-api-key",apiKey).
+        when().
+                get("/workspaces").
+        then()
+                .assertThat()
+                .statusCode(200);
+    }
+
+
+
+}
